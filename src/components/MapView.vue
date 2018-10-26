a7c899e33d98c60c905839c7d10df31bef2f6754
Gena Beraylik <>
2018-10-27 02:19:02 +0300

    Merged App

---
+++ src/components/MapView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <v-card
+    class="pa-3"
+    flat
+    height="300px"
+    img="https://cdn.vuetifyjs.com/images/toolbar/map.jpg"
+  >
+    <v-toolbar
+      dense
+      floating
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="search"
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>my_location</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    if (!this.$store.state.isUserLoggedIn) {
+      this.$router.push({
+        name: 'Login'
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/services/AuthenticationService.js
@@ -0,0 +1,11 @@
+// import Api from '@/services/Api'
+
+export default {
+  login (credentials) {
+    if (credentials.email === 'admin' && credentials.password === 'password') {
+      return 'tokenString'
+    } else {
+      return null
+    }
+  }
+}
